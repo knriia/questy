7ef89a67aaa8d31f7b2748738c2036016363d461
ivan <ialeksandrov@tender.pro>
2026-06-19 17:28:45 +0700

    setup_project

--- src/core/logger.py
+++ src/core/logger.py
@@ -1,9 +1,8 @@
 import logging
 
-
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
 logger = logging.getLogger(__name__)
