35bf3fcbbb9e78195ad299d40846a18796e55bc1
ivan <ialeksandrov@tender.pro>
2026-06-24 00:50:50 +0700

    добавлена логика создания напоминания через application слой

--- di/container.py
+++ di/container.py
@@ -2,7 +2,7 @@ from dishka import make_async_container
 
 from src.core.db.di import DbProvider
 from src.core.di import SettingsProvider
-from src.modules.activities.di import ActivityProvider
+from src.modules.activities.di import ActivityProvider, ScheduleProvider
 from src.modules.activity_records.di import ActivityRecordProvider
 from src.modules.users.di import UserProvider
 
@@ -12,4 +12,5 @@ container = make_async_container(
     UserProvider(),
     ActivityProvider(),
     ActivityRecordProvider(),
+    ScheduleProvider(),
 )
--- src/modules/activities/di.py
+++ src/modules/activities/di.py
@@ -1,8 +1,10 @@
 from dishka import Provider, Scope, provide
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.modules.activities.application.service import ActivityService
-from src.modules.activities.infrastructure.repository import ActivityRepository
+from src.modules.activities.application.services.activity import ActivityService
+from src.modules.activities.application.services.schedule import ScheduleService
+from src.modules.activities.infrastructure.repositories.activity import ActivityRepository
+from src.modules.activities.infrastructure.repositories.schedule import ScheduleRepository
 
 
 class ActivityProvider(Provider):
@@ -13,3 +15,13 @@ class ActivityProvider(Provider):
     @provide(scope=Scope.REQUEST)
     async def activity_service(self, repository: ActivityRepository) -> ActivityService:
         return ActivityService(repository=repository)
+
+
+class ScheduleProvider(Provider):
+    @provide(scope=Scope.REQUEST)
+    async def schedule_repository(self, session: AsyncSession) -> ScheduleRepository:
+        return ScheduleRepository(session)
+
+    @provide(scope=Scope.REQUEST)
+    async def schedule_service(self, repository: ScheduleRepository) -> ScheduleService:
+        return ScheduleService(repository=repository)
